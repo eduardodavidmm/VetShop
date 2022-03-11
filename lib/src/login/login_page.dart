05c3f33cca70552a3423ff94a9727a8e808657e9
DavidMaldonado0 <eduardodavidmm@hotmail.com>
2022-03-11 14:10:40 -0600

    Login Done

--- lib/src/login/login_page.dart
+++ lib/src/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vetshop/src/utils/my_colors.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -11,11 +12,98 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('VetShop Delivery'),
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          children: [
+            _imageBanner(),
+            _textFieldEmail(),
+            _textFieldPassword(),
+            _buttonLogin(),
+            _textDontHaveAccount(),
+          ],
+        ),
+      ) 
+    );
+  }
+
+  Widget _imageBanner() {
+    return Container(
+      margin: EdgeInsets.only(top: 100, bottom: MediaQuery.of(context).size.height * 0.16),
+      child: Image.asset('assets/img/delivery.png',
+      width: 200,
+      height: 200,
       ),
-      body: Text('Delivery'),
     );
   }
 }
 
+Widget _textFieldEmail() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 60, vertical: 10),
+      decoration: BoxDecoration(
+        color: MyColors.primaryOpacityColor,
+        borderRadius: BorderRadius.circular(25),
+      ),
+      child: TextField(decoration: InputDecoration(
+        hintText: 'Correo Electronico',
+        border: InputBorder.none,
+        contentPadding: EdgeInsets.all(15),
+        hintStyle: TextStyle(
+          color: MyColors.primaryColor,
+        ),
+        prefixIcon: Icon(Icons.email, color: MyColors.primaryColor,)
+      ),
+      ),
+    );
+}
+
+Widget _textFieldPassword() {
+  return Container(
+    margin: EdgeInsets.symmetric(horizontal: 60, vertical: 10),
+    decoration: BoxDecoration(
+      color: MyColors.primaryOpacityColor,
+      borderRadius: BorderRadius.circular(25),
+    ),
+    child: TextField(decoration: InputDecoration(
+        hintText: 'Contraseña',
+        border: InputBorder.none,
+        contentPadding: EdgeInsets.all(15),
+        hintStyle: TextStyle(
+          color: MyColors.primaryColor,
+        ),
+        prefixIcon: Icon(Icons.lock, color: MyColors.primaryColor,)
+    ),
+    ),
+  );
+}
+
+Widget _buttonLogin() {
+  return Container(
+    width: double.infinity,
+    margin: EdgeInsets.symmetric(horizontal: 60, vertical: 30),
+    child: ElevatedButton(onPressed: () {}, child: Text('Ingresar'),
+    style: ElevatedButton.styleFrom(
+      primary: MyColors.primaryColor,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(25)
+      ),
+      padding: EdgeInsets.symmetric(vertical: 20)
+    )),
+  );
+}
+
+Widget _textDontHaveAccount() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Text('No tienes Cuenta?'),
+      SizedBox(width: 7),
+      Text('Registrate',
+        style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: MyColors.primaryColor
+        ),),
+    ],
+  );
+}
--- lib/src/utils/my_colors.dart
+++ lib/src/utils/my_colors.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class MyColors {
 
-  static Color primaryColor = Color(0xFF26BB9E);
+  static Color primaryColor = Color(0xFF339679);
+  static Color primaryOpacityColor = Color.fromRGBO(51, 150, 121, 0.03);
 
 }
